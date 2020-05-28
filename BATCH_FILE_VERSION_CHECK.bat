8f14ad50e452d428e5392b46d7c767c478960c9c
Kyle Sanders <63331106+KSanders7070@users.noreply.github.com>
2020-05-28 01:31:44 -0400

    Update BATCH_FILE_VERSION_CHECK.bat

--- BATCH_FILE_VERSION_CHECK.bat
+++ BATCH_FILE_VERSION_CHECK.bat
@@ -11,7 +11,7 @@
 @rem Use the following as an example
 
 @rem Set your version number here. Must be same case as on the version_check page.
-SET USER_VER=1.0.1
+SET USER_VER=1.2
 
 @rem Set your version check URL here
 SET GITHUB_VERSION_CHK_URL=https://github.com/KSanders7070/BATCH_FILE_VERSION_CHECK/blob/master/Version_Check
@@ -21,9 +21,21 @@ SET BATCH_FILE_RELEASES_URL=https://github.com/KSanders7070/BATCH_FILE_VERSION_C
 
 @rem The rest of the code shouldn't have to be changed for any of your future BATCH Files.
 
+ECHO.
+ECHO.
 ECHO * * * * * * * * * * * * *
-ECHO CHECKING FOR UPDATES...
+ECHO  CHECKING FOR UPDATES...
 ECHO * * * * * * * * * * * * *
+ECHO.
+ECHO.
+
+@REM Checks to see if the Temp BATCH File from a previous update is still there. If it is, it will be deleted here.
+
+CD "%temp%"
+IF Not Exist TempBatWillDelete.bat goto MK_TEMP_FOLDER
+	DEL /Q TempBatWillDelete.bat
+
+:MK_TEMP_FOLDER
 
 If Not Exist "%temp%\VersionCheckWillDelete\" MD "%temp%\VersionCheckWillDelete"
 
@@ -42,8 +54,8 @@ CLS
 cd "%temp%"
 rd /s /q "%temp%\VersionCheckWillDelete\"
 
-START "" "%BATCH_FILE_RELEASES_URL%"
-
+ECHO.
+ECHO.
 ECHO * * * * * * * * * * * * *
 ECHO     UPDATE AVAILABLE
 ECHO * * * * * * * * * * * * *
@@ -53,18 +65,102 @@ ECHO GITHUB VERSION: %GH_VER%
 ECHO YOUR VERSION:   %USER_VER%
 ECHO.
 ECHO.
-ECHO PLEASE CLOSE THIS BATCH FILE AND GO DOWNLOAD THE MOST RECENT UPDATE AVAILABLE:
 ECHO.
-ECHO %BATCH_FILE_RELEASES_URL%
+ECHO  CHOICES:
+ECHO.
+ECHO     A   -   AUTOMATICALLY UPDATE THE BATCH FILE YOU ARE USING NOW.
+ECHO.
+ECHO     M   -   MANUALLY DOWNLOAD THE NEWEST BATCH FILE UPDATE AND USE THAT FILE.
+ECHO.
+ECHO     C   -   CONTINUE USING THIS FILE.
 ECHO.
 ECHO.
-ECHO OR YOU MAY PRESS ANY KEY TO CONTINUE USING THIS VERSION.
+ECHO.
+ECHO NOTE: IF YOU HAVE ATTMEPTED TO AUTOATMICALLY UPDATE ALREADY AND YOU CONTINUE
+ECHO       TO GET THIS UPDATE SCREEN, PLEASE UTILIZE THE MANUAL UPDATE OPTION.
+ECHO.
+ECHO.
+ECHO.
+
+:UPDATE_CHOICE
+
+SET UPDATE_CHOICE=UPDATE_METHOD_NOT_SELECTED
+
+	SET /p UPDATE_CHOICE=Please type either A, M, or C and press Enter: 
+		if /I %UPDATE_CHOICE%==A GOTO AUTO_UPDATE
+		if /I %UPDATE_CHOICE%==M GOTO MANUAL_UPDATE
+		if /I %UPDATE_CHOICE%==C GOTO CONTINUE
+		if /I %UPDATE_CHOICE%==UPDATE_METHOD_NOT_SELECTED GOTO UPDATE_CHOICE
+			echo.
+			echo.
+			echo.
+			echo.
+			echo  %UPDATE_CHOICE% IS NOT A RECOGNIZED RESPONSE. Try again.
+			echo.
+			GOTO UPDATE_CHOICE
+
+:AUTO_UPDATE
+
+CLS
+
+ECHO.
+ECHO.
+ECHO * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ECHO.
+ECHO   PRESS ANY KEY TO START THE AUTOMATIC UPDATE.
+ECHO.
+ECHO.
+ECHO   THIS SCREEN WILL CLOSE.
+ECHO.
+ECHO   WAIT 5 SECONDS
+ECHO.
+ECHO   THE NEW UPDATED BATCH FILE WILL START BY ITSELF.
+ECHO.
+ECHO * * * * * * * * * * * * * * * * * * * * * * * * * * *
 ECHO.
 ECHO.
 
 PAUSE
 
-GOTO CONTINUE
+CD "%temp%"
+
+ECHO @ECHO OFF >> TempBatWillDelete.bat
+ECHO TIMEOUT 5 >> TempBatWillDelete.bat
+ECHO CD "%~dp0" >> TempBatWillDelete.bat
+ECHO START %~nx0 >> TempBatWillDelete.bat
+ECHO EXIT >> TempBatWillDelete.bat
+
+START /MIN TempBatWillDelete.bat
+
+CD "%~dp0"
+
+powershell -Command "Invoke-WebRequest %BATCH_FILE_RELEASES_URL%/download/v%GH_VER%/%~nx0 -OutFile '%~nx0'"
+
+EXIT /b
+
+:MANUAL_UPDATE
+
+CLS
+
+START "" "%BATCH_FILE_RELEASES_URL%"
+
+ECHO.
+ECHO.
+ECHO GO TO THE FOLLOWING WEBSITE, DOWNLOAD AND USE THE LATEST VERSION OF %~nx0
+ECHO.
+ECHO.
+ECHO    %BATCH_FILE_RELEASES_URL%
+ECHO.
+ECHO.
+ECHO.
+ECHO.
+ECHO NOTE: PRESSING ANY KEY NOW WILL QUIT THIS VERSION OF THE BATCH FILE.
+ECHO.
+ECHO.
+
+PAUSE
+
+EXIT /b
 
 :RMDIR
 
@@ -75,6 +171,6 @@ rd /s /q "%temp%\VersionCheckWillDelete\"
 
 :CONTINUE
 
-ECHO Start your actual BATCH Process here.
+ECHO YOU ARE RUNNING THE MOST UP TO DATE BATCH FILE.
 
 PAUSE
